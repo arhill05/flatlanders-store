9aae46343b750822bbd3ff34d1ae88a98facf742
Andrew Hill <mdude2314@gmail.com>
2016-02-08 12:52:10 -0500

    Completed 4.4

--- app/app.js
+++ app/app.js
@@ -33,12 +33,26 @@
     };
   });
 
-  app.directive('productDescription', function(){
+  app.directive("productDescription", function(){
 		return {
 			restrict: 'E',
 			templateUrl: './product-description.html'
 		};
 	});
+    
+  app.directive("productSpecs", function(){
+      return {
+          restrict: 'A',
+          templateUrl: './product-specs.html'
+      };
+  });
+  
+  app.directive("productReviews", function(){
+      return {
+          restrict: 'E',
+          templateUrl: './product-reviews.html'
+      };
+  });
   
   var gems = [
     {
